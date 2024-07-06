5696eb7ce72050571e4116264b886f03c8c79b90
manan <daxini.manan@gmail.com>
2024-07-06 18:06:13 +0530

    done till architecture

--- HelloWorld/File.js
+++ HelloWorld/File.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const os = require("os")
 
 // Synchronous file write
 fs.writeFileSync('./manan.txt', "Heyy Manan");
@@ -37,3 +38,28 @@ console.log(fs.statSync('./manan.txt'));
 
 // Create directory synchronously
 fs.mkdirSync('my-docs/a', { recursive: true });
+
+//Blocking 
+console.log("1")
+//in blocking that is synchronous request to server the server of nodejs will execute  1 first then it will execute to read contact.txt and then will print 2 since it is blocking it will block 2 until contact is read 
+console.log(fs.readFileSync('./Contacts.txt','utf-8'))
+console.log("2")
+
+//non blocking
+
+console.log("1")
+// in non blocking it will execute  1 then wll execute 2 untill the execution of the contact.txt takes place that means it will print 1 2  and then read contact.txt it will no wait for contact.txt to execute2
+console.log(fs.readFile('./Contacts.txt','utf-8', (err,result)=>{
+    if(err){
+        console.log("error is",err)
+    }
+
+    else{
+        console.log("result is",result)
+    }
+}))
+console.log("2")
+console.log("3")
+
+
+console.log(os.cpus().length)
